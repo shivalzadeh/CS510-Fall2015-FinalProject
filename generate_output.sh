581250c89ca574afc684a115c81d57f9bb64e1ea
shiva1988 <lotfa100@mail.chapman.edu>
2015-12-15 09:33:04 -0800

    Delete generate_output.sh

--- generate_output.sh
+++
@@ -1,7 +0,0 @@
-for i in */; do
- echo "do make in $i"
- ( cd $i && make )
-done
-
-./C-JuliaSets/julia -2 2 -2 2 1000 1000 -1.037 0.17 > juliaset.csv
-./Cpp-Mandelbrot/mandelbrot -2 2 -2 2 1000 1000 > mandelbrot.csv
